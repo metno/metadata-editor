fd58fd171192a70451cf6a4944bb433ae43cadc4
Øystein Torget <oysteint@met.no>
2012-06-18 13:41:18 +0000

    Updated EditorWidget to fit with new clients side widgets and removed TextInputWidget since it was no longer needed.

--- src/main/java/no/met/metadataeditor/widget/EditorWidget.java
+++ src/main/java/no/met/metadataeditor/widget/EditorWidget.java
@@ -1,44 +1,27 @@
 package no.met.metadataeditor.widget;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Class for representing widgets. This is a pure data class and should never
  * contain any logic.
  */
-public abstract class EditorWidget {
+public class EditorWidget {
 
     // the variable name that is used for this field. Must correspond with the
     // same name in the template
-    public final String widgetId;
-   
-    // mapping between the widget variables and the current values for each widget.
-    private Map<String,String> values;
+    public final String variableName;
 
-    public EditorWidget(String widgetId) {
-        this.widgetId = widgetId;
-        this.values = new HashMap<String,String>();
-        
+    public final String widgetType;
+
+    public EditorWidget(String variableName, String widgetType) {
+        this.variableName = variableName;
+        this.widgetType = widgetType;        
     }
 
-    protected String getValue(String varName){
-        
-        if(!values.containsKey(varName)){
-            throw new IllegalArgumentException("This editor widget does not contain the variable: " + varName);
-        }
-        
-        return values.get(varName);
-    }
-    
-    protected void setValue(String varName, String value){
-        values.put(varName, value);
+    public String getVariableName() {
+        return variableName;
     }
 
-    public abstract String getType();
-
-    public String getWidgetId() {
-        return widgetId;
+    public String getWidgetType() {
+        return widgetType;
     }
-    
 }
