f29df11d47fc83da1d2ed4de9e3918f0298b3e6f
Øystein Torget <oysteint@met.no>
2012-06-28 15:14:24 +0000

    Added some more widgets.

--- src/main/java/no/met/metadataeditor/EditorConfigurationFactory.java
+++ src/main/java/no/met/metadataeditor/EditorConfigurationFactory.java
@@ -28,7 +28,7 @@ public class EditorConfigurationFactory {
         
         EditorConfiguration ec = new EditorConfiguration();
         
-        EditorWidget ew1 = new StringWidget("PIName", "PIName");
+        EditorWidget ew1 = new StringWidget("PIName", "PIname");
         ec.addWidget(ew1);
 
         EditorWidget ew2 = new StringWidget("Contact", "contact");
@@ -36,7 +36,7 @@ public class EditorConfigurationFactory {
         
         EditorWidget ew3 = new ListWidget("Keywords", "variableList");
         ec.addWidget(ew3);
-        
+      
         EditorWidget ew4 = new LatLonBoundingBoxWidget("Bounding box", "localBB");
         ec.addWidget(ew4);
 
@@ -45,7 +45,10 @@ public class EditorConfigurationFactory {
 
         EditorWidget ew6 = new UriWidget("Data ref", "dataRef");
         ec.addWidget(ew6);        
-                
+
+        EditorWidget ew7 = new ListWidget("Topic category", "topicCategory");
+        ec.addWidget(ew7);                
+        
         
         return ec;
     }
