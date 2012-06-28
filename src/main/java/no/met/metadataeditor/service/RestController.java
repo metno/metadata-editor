5081102d3bb735481a57aa6761dd5d8f6d452023
Øystein Torget <oysteint@met.no>
2012-06-28 15:07:39 +0000

    Removed handcoded dummy record.

--- src/main/java/no/met/metadataeditor/service/RestController.java
+++ src/main/java/no/met/metadataeditor/service/RestController.java
@@ -75,41 +75,7 @@ public class RestController {
             } catch (ParserConfigurationException e) {
                 e.printStackTrace();
             }
-        } else if ("12345".equals(identifier)) {
-            EditorVariable var1 = new EditorVariable(new StringAttributes());
-            EditorVariableContent cont1 = new EditorVariableContent();
-            cont1.setAttrs(new StringAttributes("Hans Klaus"));
-            var1.addContent(cont1);
-            varMap.put("contact", var1);
-
-            EditorVariableContent cont2 = new EditorVariableContent();
-            EditorVariable var2 = new EditorVariable(new StringAttributes());
-            cont2.setAttrs(new StringAttributes("Metadata editor"));
-            var2.addContent(cont2);
-            varMap.put("PIName", var2);
-
-            EditorVariable var3 = new EditorVariable(new StringAttributes());
-            EditorVariableContent cont3_1 = new EditorVariableContent();
-            cont3_1.setAttrs(new StringAttributes("precipitation"));
-            var3.addContent(cont3_1);
-            EditorVariableContent cont3_2 = new EditorVariableContent();
-            cont3_2.setAttrs(new StringAttributes("millimeter"));
-            var3.addContent(cont3_2);
-            varMap.put("variableList", var3);
-
-            EditorVariableContent cont4 = new EditorVariableContent();
-            LatLonBBAttributes llbb = new LatLonBBAttributes();
-            EditorVariable var4 = new EditorVariable(llbb);
-            llbb.setEast(180);
-            llbb.setWest(-180);
-            llbb.setNorth(90);
-            llbb.setSouth(60);
-            cont4.setAttrs(llbb);
-            var4.addContent(cont4);
-            varMap.put("llbb", var4);
-
-        }
-
+        } 
 
         try {
             ObjectMapper mapper = new ObjectMapper();
