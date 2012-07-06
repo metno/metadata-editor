2736330775fa60e13bce8cb08660ce13d260e216
Øystein Torget <oysteint@met.no>
2012-07-06 14:08:51 +0000

    Removed getPredefinedInstance() since it was no longer in use.

--- src/main/java/no/met/metadataeditor/EditorConfigurationFactory.java
+++ src/main/java/no/met/metadataeditor/EditorConfigurationFactory.java
@@ -8,12 +8,6 @@ import javax.xml.bind.Unmarshaller;
 
 import no.met.metadataeditor.datastore.DataStore;
 import no.met.metadataeditor.datastore.DataStoreFactory;
-import no.met.metadataeditor.widget.EditorWidget;
-import no.met.metadataeditor.widget.LatLonBoundingBoxWidget;
-import no.met.metadataeditor.widget.StartAndStopTimeWidget;
-import no.met.metadataeditor.widget.StringWidget;
-import no.met.metadataeditor.widget.ListWidget;
-import no.met.metadataeditor.widget.UriWidget;
 
 
 /**
@@ -50,33 +44,4 @@ public class EditorConfigurationFactory {
                 
     }
     
-    
-    public static EditorConfiguration getPredefinedInstance(){
-        
-        EditorConfiguration ec = new EditorConfiguration();
-        
-//        EditorWidget ew1 = new StringWidget("PIName", "PIname");
-//        ec.addWidget(ew1);
-//
-//        EditorWidget ew2 = new StringWidget("Contact", "contact");
-//        ec.addWidget(ew2);
-//        
-//        EditorWidget ew3 = new ListWidget("Keywords", "variableList");
-//        ec.addWidget(ew3);
-//      
-//        EditorWidget ew4 = new LatLonBoundingBoxWidget("Bounding box", "globalBB");
-//        ec.addWidget(ew4);
-//
-//        EditorWidget ew5 = new StartAndStopTimeWidget("Time extent", "timeExtend");
-//        ec.addWidget(ew5);        
-//
-//        EditorWidget ew6 = new UriWidget("Data ref", "dataRef");
-//        ec.addWidget(ew6);        
-//
-//        EditorWidget ew7 = new ListWidget("Topic category", "topicCategory");
-//        ec.addWidget(ew7);                
-        
-        
-        return ec;
-    }
 }
