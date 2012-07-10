7476d617b37eeed889b4e7dae1325a66e8c3fc1f
Øystein Torget <oysteint@met.no>
2012-07-10 13:33:33 +0000

    Added size and maxlength support to the StringWidget.

--- src/main/java/no/met/metadataeditor/widget/StringWidget.java
+++ src/main/java/no/met/metadataeditor/widget/StringWidget.java
@@ -3,11 +3,15 @@ package no.met.metadataeditor.widget;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAttribute;
 
 public class StringWidget extends EditorWidget {
 
     private static final long serialVersionUID = -7542890498259559947L;
+        
+    private int size = 40;
+    
+    private int maxlength = 40;
 
     public StringWidget(){
         super();
@@ -23,6 +27,26 @@ public class StringWidget extends EditorWidget {
         Map<String,String> defaultValue = new HashMap<String,String>();
         defaultValue.put("str", "");
         return defaultValue;
+    }
+
+    @XmlAttribute
+    public int getSize() {
+        return size;
+    }
+
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @XmlAttribute
+    public int getMaxlength() {
+        return maxlength;
+    }
+
+
+    public void setMaxlength(int maxlength) {
+        this.maxlength = maxlength;
     }
 
 }
