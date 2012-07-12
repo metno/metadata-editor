16f7188ab5f7b5ff63e444631b1b68ac6c7d397a
Øystein Torget <oysteint@met.no>
2012-07-12 13:51:32 +0000

    Added support for size and maxlength in the URI widget.

--- src/main/java/no/met/metadataeditor/widget/UriWidget.java
+++ src/main/java/no/met/metadataeditor/widget/UriWidget.java
@@ -3,16 +3,18 @@ package no.met.metadataeditor.widget;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="uri")
 public class UriWidget extends EditorWidget {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = -1244778870767868773L;
 
+    private int size = 100;
+    
+    private int maxlength = 400; 
+    
     public UriWidget(){
         super();
     }
@@ -28,5 +30,23 @@ public class UriWidget extends EditorWidget {
         return defaultValue;
     }
 
+    @XmlAttribute
+    public int getSize() {
+        return size;
+    }
 
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    @XmlAttribute
+    public int getMaxlength() {
+        return maxlength;
+    }
+
+
+    public void setMaxlength(int maxlength) {
+        this.maxlength = maxlength;
+    }
 }
