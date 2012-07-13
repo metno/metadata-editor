151ce2af00b5ea491348b6ea2b3b8fb99009ea41
Øystein Torget <oysteint@met.no>
2012-07-13 07:29:52 +0000

    Replaced the use of property files with the Config class for configuration.

--- src/main/java/no/met/metadataeditor/datastore/DataStoreFactory.java
+++ src/main/java/no/met/metadataeditor/datastore/DataStoreFactory.java
@@ -1,42 +1,28 @@
 package no.met.metadataeditor.datastore;
 
-import java.io.IOException;
-import java.util.Properties;
+import no.met.metadataeditor.Config;
 
 public class DataStoreFactory {
 
     
     public static DataStore getInstance() {
         
+        Config config = new Config("/metadataeditor.properties", Config.ENV_NAME);
         
-        Properties prop = new Properties();
-        try {
-            prop.load(DataStore.class.getResourceAsStream("/metadataeditor.properties"));
-        } catch (IOException e) {            
-            throw new RuntimeException("Cannot open property file: " + e.getMessage());
-        }
-        
-        if(prop.containsKey("datastore.type")){
-            
-            String datastore = prop.getProperty("datastore.type");            
-            if( "DiskDataStore".equalsIgnoreCase(datastore)){
-               
-                String path;
-                if(prop.containsKey("datastore.path")){
-                    path = prop.getProperty("datastore.path");
-                } else {
-                    path = System.getProperty("java.io.tmpdir");
-                }
-                
-                return new DiskDataStore(path);
+        String datastore = config.getRequired("datastore.type");
+        if( "DiskDataStore".equalsIgnoreCase(datastore)){
+           
+            String path;
+            if( config.get("datastore.path") != null ){
+                path = config.get("datastore.path");
+            } else {
+                path = System.getProperty("java.io.tmpdir");
             }
-        } else {
-            throw new RuntimeException("Missing 'datastore' in config file.");
+            
+            return new DiskDataStore(path);
         }
         
         return null;
         
     }
-    
-    
 }
