154d2731d5bc3559135f354e4a003b973bc7b3ad
Øystein Torget <oysteint@met.no>
2012-08-08 07:07:47 +0000

    Added ignoring enter keys also for input fields that are added using Ajax calls.

--- src/main/webapp/resources/javascript/utils.js
+++ src/main/webapp/resources/javascript/utils.js
@@ -3,3 +3,15 @@ function ignore_enter_key () {
         event.preventDefault();
     }
 }
+
+
+function input_field_added ( data ) {
+    
+    // when a new input field is added we need to disable the enter key in
+    // this field as well. Since we do not have access to the exact field here
+    // we do the brute force approach and just reapply it to all input fields.
+    if( data.status == "success" ){
+        jQuery('input').keypress(ignore_enter_key);
+    }
+    
+}
