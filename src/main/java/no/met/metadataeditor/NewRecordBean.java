7b158d0c300d2f5f2b346186d2327626736e25f3
Øystein Torget <oysteint@met.no>
2012-08-13 09:52:59 +0000

    Added support for adding a new metadata record.

---
+++ src/main/java/no/met/metadataeditor/NewRecordBean.java
@@ -0,0 +1,134 @@
+package no.met.metadataeditor;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import no.met.metadataeditor.dataTypes.EditorTemplate;
+import no.met.metadataeditor.dataTypes.EditorVariableContent;
+import no.met.metadataeditor.datastore.DataStore;
+import no.met.metadataeditor.datastore.DataStoreFactory;
+import no.met.metadataeditor.datastore.SupportedFormat;
+
+@ManagedBean
+@ViewScoped
+public class NewRecordBean implements Serializable {
+
+    private static final Logger logger = Logger.getLogger(NewRecordBean.class.getName());
+    
+    // automatically set based on the query parameters
+    private String project;    
+    
+    // the identifier for a new record. Used when the user clicks "New"
+    private String newRecordIdentifier;
+    
+    // the format for a new record. Used when the user clicks "New"
+    private SupportedFormat newRecordFormat;    
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6284081275030111665L;
+
+    
+    /**
+     * Validate that the record identifier is has not been used within a project.
+     * @param context Current context
+     * @param component The originating component
+     * @param value The new identifier value
+     * @throws ValidatorException Throw if the record identifier in value is already in use
+     */
+    public void validateRecordIdentifier(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        String identifier = (String) value;
+        
+        String validIdentifier = "[\\w!\"#$%&'()*+,-\\.:;<=>?@{\\|}]+";        
+        if( !identifier.matches(validIdentifier)){
+            FacesMessage message = new FacesMessage("Record identifier is not valid.", "Record identifier contains invalid characters. Please restrict to normal english letter, number and punctiation characters.");
+            message.setSeverity(FacesMessage.SEVERITY_FATAL);
+            throw new ValidatorException(message);
+        }
+        
+        
+        DataStore datastore = DataStoreFactory.getInstance();        
+        if( datastore.metadataExists(project, identifier)){
+            
+            FacesMessage message = new FacesMessage("Record identifier already in use.", "The record identifier is already in use and cannot be reused for a new record.");
+            message.setSeverity(FacesMessage.SEVERITY_FATAL);
+            throw new ValidatorException(message);
+        }                
+    }      
+    
+    public List<SupportedFormat> getSupportedFormats() {
+        
+        DataStore datastore = DataStoreFactory.getInstance();
+        return datastore.getSupportedFormats(project);
+        
+    }
+    
+    public String newRecord() {
+        
+        DataStore datastore = DataStoreFactory.getInstance();
+        
+        // get the XML template for the format form the datastore
+        
+        String templateXML = datastore.readTemplate(project, newRecordFormat);
+        try {
+            EditorTemplate template = new EditorTemplate(new InputSource(new StringReader(templateXML)));
+            Document emptyRecord = template.writeContent(new HashMap<String, List<EditorVariableContent>>());
+            datastore.writeMetadata(project, newRecordIdentifier, EditorUtils.docToString(emptyRecord));
+        } catch (SAXException e) {           
+            logger.log(Level.SEVERE, "Failed to parse template", e);
+            throw new EditorException("Failed to parse template", e);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Failed to read template", e);
+            throw new EditorException("Failed to read template", e);
+        } catch (JDOMException e) {
+            logger.log(Level.SEVERE, "Failed to write empty template", e);
+            throw new EditorException("Failed to write template", e);            
+        }        
+        
+        return "editor.xhtml?project=" + project + "&record=" + newRecordIdentifier + "&faces-redirect=true";
+    }
+
+    public String getNewRecordIdentifier() {
+        return newRecordIdentifier;
+    }
+
+    public void setNewRecordIdentifier(String newRecordIdentifier) {
+        this.newRecordIdentifier = newRecordIdentifier;
+    }
+
+    public SupportedFormat getNewRecordFormat() {
+        return newRecordFormat;
+    }
+
+    public void setNewRecordFormat(SupportedFormat newRecordFormat) {
+        this.newRecordFormat = newRecordFormat;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+        
+}
