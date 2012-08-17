63091655de7ee5eba4b5ca25290fc624df5960cd
Øystein Torget <oysteint@met.no>
2012-08-17 10:28:17 +0000

    Removed unnecessary newInstance() function.

--- src/main/java/no/met/metadataeditor/dataTypes/attributes/NullAttributes.java
+++ src/main/java/no/met/metadataeditor/dataTypes/attributes/NullAttributes.java
@@ -3,8 +3,4 @@ package no.met.metadataeditor.dataTypes.attributes;
 
 public class NullAttributes extends DataAttributes {
 
-    public DataAttributes newInstance() {
-        return new NullAttributes();
-    }
-
 }
