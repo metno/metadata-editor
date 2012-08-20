d0dde573da284c21f2d3c6172492e583fd5f885e
Øystein Torget <oysteint@met.no>
2012-08-20 08:25:04 +0000

    Removed un-used imports.

--- src/main/java/no/met/metadataeditor/dataTypes/TemplateHandler.java
+++ src/main/java/no/met/metadataeditor/dataTypes/TemplateHandler.java
@@ -10,8 +10,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 import no.met.metadataeditor.dataTypes.attributes.DataAttribute;
